c1840cc351f8cbf94f951d5ed39f41e11f9a12e1
Lee Ryan <2solimanleeryan@gmail.com>
2023-01-09 19:33:31 +0800

    Create User model

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    first_name: { type:String, required: true },
+    last_name: { type: String, required: true },
+    display_name: { type: String, required: true },
+    email_address: { type: String, required: true },
+    joined_date: { type: Date, required: true },
+    display_picture: { type: String, required: true }
+});
+
+UserSchema.virtual('name').get(function() {
+    return `${this.first_name} ${this.last_name}`;
+});
+
+module.exports = mongoose.model('User');
