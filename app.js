561669a603fae5b16b26e11b0dd28bff103389b9
Lee Ryan <2solimanleeryan@gmail.com>
2023-01-09 14:08:26 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const session = require('express-session');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth');
+
+const path = require('path');
+
+// Routes
+const indexRouter = require('./routes/index');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use('/', indexRouter);
+
+app.listen(3000);
