0e2263ca8ef162764be749fe42a40533f40e4000
Lee Ryan <2solimanleeryan@gmail.com>
2023-01-10 22:21:53 +0800

    Set default of display name to anonymous

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     first_name: { type:String, required: true },
     last_name: { type: String, required: true },
-    display_name: { type: String, required: true },
+    display_name: { type: String, default: 'Anonymous', required: true },
     email_address: { type: String, required: true },
     joined_date: { type: Date, required: true },
     display_picture: { type: String, required: true }
