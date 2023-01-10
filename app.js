4dc36f36be935b02c43e2f3a4df2c275f3bada0f
Lee Ryan <2solimanleeryan@gmail.com>
2023-01-10 09:20:22 +0800

    Allow log in via google

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const session = require('express-session');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth');
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
 
 const path = require('path');
+const dotenv = require('dotenv').config();
 
 // Routes
 const indexRouter = require('./routes/index');
@@ -13,6 +14,30 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(express.static('public'));
+
+passport.use(
+    new GoogleStrategy({
+        clientID: process.env.clientID,
+        clientSecret: process.env.clientSecret,
+        callbackURL: '/auth/google/callback'
+    }, (accessToken, refreshToken, profile, done) => {
+        done(null, profile);
+    })
+);
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
+app.use(session({secret: 'secret', resave: false, saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 
 app.use('/', indexRouter);
 
