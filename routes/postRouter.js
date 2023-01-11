17d9c89aeb21c251ff6773fb72579a71f7d5b2bb
Lee Ryan <2solimanleeryan@gmail.com>
2023-01-11 17:41:46 +0800

    Make user only authenticated users can post

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+
 const postController = require('../controllers/postController');
 
-router.get('/create', postController.post_create_get);
+// Borrow logged_in function from userController.
+const userController = require('../controllers/userController');
 
-router.post('/create', postController.post_create_post);
+router.get('/create', userController.logged_in, postController.post_create_get);
+
+router.post('/create', userController.logged_in, postController.post_create_post);
 
 module.exports = router;
