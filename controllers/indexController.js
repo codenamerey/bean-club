5f6f7d6e6c55b17bfb5c91bf07f7ed5ec0d6fe2e
Lee Ryan <2solimanleeryan@gmail.com>
2023-01-11 18:52:14 +0800

    Display posts from Bean club members in home

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,9 +1,31 @@
 const User = require('../models/user');
+const Post = require('../models/post');
+
+const async = require('async');
 
 exports.index = (req, res, next) => {
-    User.find({}, (err, users) => {
-        if(err) next(err);
 
-        res.render('index', {title: "The Bean Club", users});
-    });
+    async.parallel({
+        users(callback) {
+            User.find({}, (err, users) => {
+                if(err) return callback(err, null);
+
+                callback(null, users);
+            })
+        },
+
+        posts(callback) {
+            Post.find({})
+                .sort({creation_date: -1}).
+                exec((err, posts) => {
+                    if(err) return callback(err, null);
+
+                    callback(null, posts);
+                })
+        }
+    }, (err, results) => {
+        if(err) return next(err);
+
+        res.render('index', { title: "The Bean Club", users: results.users, posts: results.posts });
+    })
 }
