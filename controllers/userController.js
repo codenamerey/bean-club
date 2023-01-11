9df6ea12a336f8fce22dda12b1535849cf148d92
Lee Ryan <2solimanleeryan@gmail.com>
2023-01-11 10:48:01 +0800

    Validate user input before updating to database

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const User = require('../models/user');
+const { body, validationResult } = require('express-validator');
 
 exports.logged_in = (req, res, next) => {
     if(req.user) {
@@ -26,13 +27,28 @@ exports.display_name_edit_get = (req, res, next) => {
     res.render('display-name-edit', {title: "Edit Display Name"});
 }
 
-exports.display_name_edit_post = (req, res, next) => {
-    User.findByIdAndUpdate(req.user.id, {display_name: req.body.display_name}, (err, result) => {
-        if(err) return next(err);
-    });
+exports.display_name_edit_post = [ 
 
-    res.redirect('/user');
-}
+    body('display_name', 'Display name is required').trim()
+                        .isAlphanumeric()
+                        .withMessage('Display name has to be alphanumeric')
+                        .isLength({min: 1})
+                        .escape(),
+
+    (req, res, next) => {
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()) {
+            // There are errors, re-render page again.
+            res.render('display-name-edit', {title: "Edit Display Name", errors: errors.array()});
+        } else {
+            User.findByIdAndUpdate(req.user.id, {display_name: req.body.display_name}, (err, result) => {
+                if(err) return next(err);
+            });
+
+            res.redirect('/user');
+        }
+}];
 
 exports.logout = (req, res, next) => {
     req.logout((err) => {
